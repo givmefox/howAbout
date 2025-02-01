e5adeb23693710c666e9a699fe50606a506aea05
givemefox <jhkang02000@gmail.com>
2025-02-01 15:09:29 +0900

    db_config 수정

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,26 @@
 const mysql = require('mysql2');
-require('dotenv').config();
+const dotenv = require('dotenv');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+dotenv.config();
+
+
+//mysql 연결
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,  // DB서버 IP주소
+  user: process.env.DB_USER,  // DB접속 아이디
+  password: process.env.DB_PASSWORD,  // DB암호
+  database: process.env.DB_DATABASE,  //사용할 DB명
+  port: process.env.DB_PORT // DB 포트
 });
 
-module.exports = pool.promise();
+db.connect((err) => {
+  if (err) {
+    console.error('mysql connection error');
+    console.error(err);
+    throw err;
+  } else {
+    console.log('mysql connection success');
+  }
+});
+
+module.exports = db;
