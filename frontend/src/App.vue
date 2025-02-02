5e08607c5fbe0683e2268daec588896fb60b406d
spark0909 <star10909@naver.com>
2025-02-02 19:17:40 +0900

    Fix: .gitignore 추가 및 변경 사항 반영

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default{
+  name: 'App',
+  data() {
+  },
+  methods: {
+  },
+  components: {
+    Navbar: Navbar,
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1, h2, h3 {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 0.5rem;
+}
+
+button {
+  margin-right: 10px;
+}
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+        <nav class="navbar">
+            <a href="#" class="logo">여기어때</a>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Chart</a>
+                <a href="#">About</a>
+                <button class="login-btn">Login</button>
+            </div> 
+        </nav>
+</template>
+    
+<script>
+    export default{
+        name: 'NavbarComponent',
+    }
+</script>
+    
+<style>
+    .navbar{
+        display: flex;
+        background: #dd3333;
+        padding: 20px;
+        justify-content: space-between; 
+        align-items: center;
+    }
+
+    .navbar a{
+        color:  #fff;
+        text-decoration: none;
+        padding: 1em;
+    }
+
+    .logo {
+        font-size: 22px;
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+    }
+
+    .login-btn {
+        background-color: white;
+        color: #dd3333;
+        border: none;
+        padding: 10px 15px;
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+</style>
